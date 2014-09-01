3baac998fc258027e1f8947812c706dd2f79b555
gringogidget <kristinagroeger@gmail.com>
2014-09-01 14:45:23 -0400

    idk wtf i'm doing haha
    heueheueheu

--- transformer.js
+++ transformer.js
@@ -5,4 +5,20 @@ module.exports = function (imageBuffer) {
 
   return imageBuffer;
 
+// idk wtf I'm doing: 
+
+  -seed 
+
+  $ composite -gravity /images/3_spooky_skelly.gif
+  $ composite -gravity /images/candles.gif
+  $ composite -gravity /images/ghosts.gif
+  $ composite -gravity /images/laff_it_up_ghost.gif
+  $ composite -gravity /images/lightning.gif
+  $ composite -gravity /images/lightning02.gif
+  $ composite -gravity /images/lightning03.gif
+  $ composite -gravity /images/mario_ghost.gif
+  $ composite -gravity /images/posessed.gif
+  $ composite -gravity /images/scary_girl_clean.gif
+
+
 };
